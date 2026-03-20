b43dd7229cb055f053f3a9583402bd952564c03c
paarturnax <paarturnax.bg@gmail.com>
2026-03-20 10:58:17 +0300

    auto shooting done. not implemented

--- Assets/AutoShooting.cs
+++ Assets/AutoShooting.cs
@@ -6,13 +6,55 @@ public class AutoShooting : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float cooldown;
 
-    //private ZombieMovement FindNearesZombie()
-    //{
-    //    return 
-    //}
+    [SerializeField] private int bullets;
+    private float timer = 0f;
 
-    private void Shoot()
+    private void Update()
     {
+        timer += Time.deltaTime;
 
+        if (bullets <= 0 && timer >= cooldown)
+        {
+            ZombieMovement zombie = FindNearesZombie();
+            Shoot(zombie);
+            timer = 0f;
+            bullets--;
+        }
+    }
+
+    private void Shoot(ZombieMovement zombie)
+    {
+        GameObject projectile = Instantiate(projectilePrefab);
+        Setup(projectile.transform, zombie.transform);
+    }
+
+    private void Setup(Transform projectile, Transform zombie)
+    {
+        projectile.position = transform.position;
+        Vector3 direction = zombie.position - transform.position;
+        projectile.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+    }
+
+    private ZombieMovement FindNearesZombie()
+    {
+        Vector3 position = transform.position;
+        if (spawner.SpawnedZombies.Count == 0)
+        {
+            return null;
+        }
+
+        ZombieMovement nearest = spawner.SpawnedZombies[0];
+        float minDistance = Vector3.Distance(position, nearest.transform.position);
+        for (int i = 1; i < spawner.SpawnedZombies.Count; i++)
+        {
+            ZombieMovement zombie = spawner.SpawnedZombies[i];
+            float distance = Vector3.Distance(position, zombie.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = zombie;
+            }
+        }
+        return nearest;
     }
 }
